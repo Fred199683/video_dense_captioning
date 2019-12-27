3759783ad0bc7a5ee20cd40b6e1fb29eb9fbbe76
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-12-28 00:36:15 +0900

    fix prepro

--- prepro.py
+++ prepro.py
@@ -108,10 +108,11 @@ def main():
 
         with h5py.File(cfg.DATASET.RAW_FEATURE_PATH) as f_features:
             for video_id in tqdm(captions_data.keys()):
-                video_feature = f_features[video_id]['c3d_features']
-                print(type(video_feature))
-
+                video_feature = f_features[video_id]['c3d_features'].value
                 feature_size = video_feature.shape[0]
+                video_feature = np.pad(video_feature, ((0, 0), (0, (4 - (feature_size % 4)) % 4)))
+                video_feature = np.mean(video_feature.reshape(video_feature.shape[0] // 4, -1, video_feature.shape[1]), axis=1)
+
                 video_duration = captions_data[video_id]['duration']
                 event_timestamps = captions_data[video_id]['timestamps']
 
@@ -120,8 +121,8 @@ def main():
                 os.makedirs(video_feature_path)
 
                 for i, (begin_timestamp, end_timestamp) in enumerate(event_timestamps):
-                    begin_pivot = round(begin_timestamp / video_duration * feature_size)
-                    end_pivot = round(end_timestamp / video_duration * feature_size)
+                    begin_pivot = round(begin_timestamp / video_duration * feature_size / 4)
+                    end_pivot = round(end_timestamp / video_duration * feature_size / 4)
 
                     event_feature = video_feature[begin_pivot: end_pivot, :]
                     np.save(os.path.join(video_feature_path, '%d.npy' % i), event_feature)
