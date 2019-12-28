45af3d9eca4c82c4684792cbd68b12ef97ec576d
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-12-28 21:35:04 +0900

    fix bugs

--- core/model.py
+++ core/model.py
@@ -116,8 +116,6 @@ class CaptionRNN(nn.Module):
 
         # Trainable parameters :
         self.lstm_cell = nn.LSTM(self.D + self.M, self.H, dropout=0.5)
-        self.hidden_state_init_layer = nn.Linear(self.D, self.H)
-        self.cell_state_init_layer = nn.Linear(self.D, self.H)
         self.embedding_lookup = nn.Embedding(self.V, self.M)
         self.feats_proj_layer = nn.Linear(self.D, self.D)
         self.hidden_to_attention_layer = nn.Linear(self.H, self.D)
@@ -133,13 +131,8 @@ class CaptionRNN(nn.Module):
         self.features_norm_layer = nn.LayerNorm(self.D)
         self.dropout = nn.Dropout(p=self.dropout)
 
-    def get_initial_lstm(self, feats_proj):
-        feats_mean = torch.mean(feats_proj, 1)
-        h = torch.tanh(self.hidden_state_init_layer(feats_mean)).unsqueeze(0)
-        c = torch.tanh(self.cell_state_init_layer(feats_mean)).unsqueeze(0)
-        return c, h
-    #def zero_hidden_states(self, batch_size):
-    #    return torch.zeros(batch_size)
+    def zero_hidden_states(self, batch_size):
+        return torch.zeros(batch_size, self.H)
 
     def project_features(self, features):
         batch, event_num, event_len, feature_dim = features.size()
