ebd26e853139541376c28bd739c6a18f83a7504a
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-12-30 20:13:41 +0900

    debugging

--- core/solver.py
+++ core/solver.py
@@ -262,6 +262,7 @@ class CaptioningSolver(object):
         captions_masks = captions_masks.to(device=self.device)
         batch_sizes = batch_sizes.to(device=self.device)
         cap_vecs = cap_vecs.to(device=self.device)
+        print(cap_vecs)
 
         caption_features = self.caption_rnn.normalize(caption_features)
         caption_features_proj = self.caption_rnn.project_features(caption_features)
@@ -294,7 +295,7 @@ class CaptioningSolver(object):
                 loss += self.word_criterion(logits, next_cap_vecs)
 
                 mask_next_cap_vecs = (next_cap_vecs != self._null)
-                print(caption_idx, mask_next_cap_vecs)
+                #print(caption_idx, mask_next_cap_vecs)
                 acc += torch.sum((torch.argmax(logits, dim=-1) == next_cap_vecs) * mask_next_cap_vecs).item()
                 count_mask += torch.sum(mask_next_cap_vecs).item()
                 # feats_alphas.append(feats_alpha)
