c71015fe40a24935ab7ff038210e4312e4bfa442
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-12-31 01:45:30 +0900

    debugging

--- core/solver.py
+++ core/solver.py
@@ -263,7 +263,6 @@ class CaptioningSolver(object):
         captions_masks = captions_masks.to(device=self.device)
         batch_sizes = batch_sizes.to(device=self.device)
         cap_vecs = cap_vecs.to(device=self.device)
-        print(sentences)
 
         caption_features = self.caption_rnn.normalize(caption_features)
         caption_features_proj = self.caption_rnn.project_features(caption_features)
@@ -298,9 +297,8 @@ class CaptioningSolver(object):
                 mask_next_cap_vecs = (next_cap_vecs != self._null)
                 print(caption_idx, mask_next_cap_vecs, next_cap_vecs)
                 for b_sents in sentences[:batch_size]:
-                    for b_sent in b_sents[event_idx]:
-                        for w in b_sent[caption_idx]:
-                            print(w, end=' ')
+                    for sent in b_sents[event_idx]:
+                        print(sent[caption_idx], end=' ')
                 print()
                 acc += torch.sum((torch.argmax(logits, dim=-1) == next_cap_vecs) * mask_next_cap_vecs).item()
                 count_mask += torch.sum(mask_next_cap_vecs).item()
