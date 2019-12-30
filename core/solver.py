1bfc313b87c95b9eb3044d8c27e3e218ead99623
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2019-12-31 03:10:43 +0900

    fix bugs

--- core/solver.py
+++ core/solver.py
@@ -36,14 +36,7 @@ def train_collate(batch):
     padded_batch_cap_vecs = torch.zeros(batch_size, max_event_num, caption_length).long()
     for i, event_cap_vecs in enumerate(batch_cap_vecs):
         for j, cap_vecs in enumerate(event_cap_vecs):
-            try:
-                padded_batch_cap_vecs[i][j] = torch.tensor(cap_vecs).long()
-            except:
-                print('-' * 50)
-                print('Debug log: ')
-                print(i, j)
-                print(batch_size, max_event_num, caption_length)
-                print(len(event_cap_vecs))
+            padded_batch_cap_vecs[i][j] = torch.tensor(cap_vecs).long()
 
     padded_batch_event_features = torch.zeros(batch_size, max_event_num, feature_dim)
     for i, event_features in enumerate(batch_features):
@@ -340,7 +333,8 @@ class CaptioningSolver(object):
             engine.state.scores = caption_scores
 
     def _test(self, engine, batch):
-        self.model.eval()
+        self.event_rnn.eval()
+        self.caption_rnn.eval()
 
         caption_features, event_features, events_mask, captions_masks, batch_sizes, video_ids = batch
         caption_features = caption_features.to(device=self.device)
