ea0fd1f64a9f4bcc3e1eacdb2bb971d8f1a397ca
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-01-02 00:27:30 +0900

    fix bugs

--- core/utils.py
+++ core/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import json
 import time
 import os
-from densevid_eval.evaluate import ANETcaptions
+import subprocess
 
 
 def decode_captions(captions, idx_to_word):
@@ -47,27 +47,5 @@ def save_json(data, path):
 
 
 def evaluate(candidate_path='./data/val/val.candidate.captions.json', references_path=['data/val_1.json', 'data/val_2.json'], get_scores=False):
-    evaluator = ANETcaptions(ground_truth_filenames=references_path,
-                            prediction_filename=candidate_path,
-                            tious=[0.3, 0.5, 0.7, 0.9],
-                            max_proposals=1000,
-                            verbose=False)
-    evaluator.evaluate()
-
-    # Output the results
-    # if args.verbose:
-    #     for i, tiou in enumerate(args.tious):
-    #         print '-' * 80
-    #         print "tIoU: " , tiou
-    #         print '-' * 80
-    #         for metric in evaluator.scores:
-    #             score = evaluator.scores[metric][i]
-    #             print '| %s: %2.4f'%(metric, 100*score)
-
-    # Print the averages
-    # print('-' * 80)
-    # print('Average across all tIoUs')
-    # print('-' * 80)
-    # for metric in evaluator.scores:
-    #     score = evaluator.scores[metric]
-    #     print('| %s: %2.4f' % (metric, 100 * sum(score) / float(len(score))))
+    python = '/home/nii/anaconda3/envs/py27/bin/python'
+    subprocess.call(['cd', 'densevid_eval', '&&', python, 'evaluate.py', '-s', os.path.abspath(candidate_path)])
